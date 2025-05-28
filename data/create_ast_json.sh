26dc536390be120e037ee74e31aeb26bf583fdc8
Sunjae Kim <ksj20.git@gmail.com>
2025-05-28 12:51:20 +0900

    delete ast tree related code

--- data/create_ast_json.sh
+++
@@ -1,1 +0,0 @@
-clang++-18 -Xclang -ast-dump=json -fsyntax-only ../src/main.cpp > main.json
--- data/src/main7.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-
-int main() {
-    using namespace std;
-    cout  << "Hello" <<  endl;
-    return 0;
-}
